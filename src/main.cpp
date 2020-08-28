fbfed577dccee6bae543ac91e4115be23bd957ab
Shahan Neda <shahan.neda@gmail.com>
2020-08-27 22:31:42 -0700

    made simple gl calls

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,8 @@ NCraftMain::NCraftMain(){
 }
 
 
+unsigned int shaderProgram;
+unsigned int VAO; // vao basically stores the config of the vbo
 void NCraftMain::initOpenGL(){
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
@@ -26,7 +28,92 @@ void NCraftMain::initOpenGL(){
   glViewport(0, 0, NCraftWindow::WIDTH, NCraftWindow::HEIGHT); 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
-  processInput(window);
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f
+  };  
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+  /*
+   *GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+GL_STATIC_DRAW: the data is set only once and used many times.
+GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+   *
+   *
+   */
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+  const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+
+  const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  //check for errors
+  int  success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if(!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+
+
+
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if(!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+  glUseProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);  
+
+
+  glGenVertexArrays(1, &VAO);  
+
+  glBindVertexArray(VAO);
+  // which location / size of each vec (3) / type float/ normalized/ stride/starting point
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);  // this is the location
+
+
+
 
 } 
 void NCraftMain::mainLoop(){
@@ -34,8 +121,11 @@ void NCraftMain::mainLoop(){
   {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    processInput(window);
 
-
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
