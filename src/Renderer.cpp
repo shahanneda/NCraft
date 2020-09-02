988d3b99c2754e5f33506429e3e759ffc6629c28
Shahan Neda <shahan.neda@gmail.com>
2020-09-02 00:07:48 -0700

    setup rotating square

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -64,7 +64,7 @@ void Renderer::Render()
   mainShader->Bind();
 
   glm::mat4 trans = glm::mat4(1.0f);
-  trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+  trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
   trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
 
   uint32_t transformLoc = glGetUniformLocation(mainShader->id, "transform");
