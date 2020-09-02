155ba041d2182ca06f0d98e8d604072895168dde
Shahan Neda <shahan.neda@gmail.com>
2020-09-02 01:07:56 -0700

    got semi working cube

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -8,6 +8,25 @@ using glm::vec2;
 using glm::vec3;
 // #include <Texture.h>
 
+std::vector<int> indices = {
+    0, 1, 2, //  front face
+    2, 3, 0, //
+
+    2, 6, 7, // right side
+    2, 7, 3, //
+
+    6, 5, 4, // back face
+    5, 4, 7, //
+
+    5, 4, 0, // left side
+    5, 0, 1, //
+
+    1, 2, 6, // top side
+    1, 6, 5, //
+
+    0, 3, 7, // bottom side
+    1, 7, 4  //
+};
 // glBindBuffer(GL_ARRAY_BUFFER, VBO);
 Renderer::Renderer(GLFWwindow **window)
 {
@@ -16,19 +35,29 @@ Renderer::Renderer(GLFWwindow **window)
   mainShader = new Shader("shaders/shader.vert", "shaders/shader.frag");
 
   std::vector<vec3> vertices = {
-      vec3(0.5f, 0.5f, 0.0f),
-      vec3(0.5f, -0.5f, 0.0f),
-      vec3(-0.5f, -0.5f, 0.0f),
-      vec3(-0.5f, 0.5f, 0.0f)};
-  std::vector<int> indices = {
-      0, 1, 3,
-      1, 2, 3};
+      vec3(0, 0, 0), // 0 bottom left
+      vec3(0, 1, 0), // 1 top left
+      vec3(1, 1, 0), // 2 top right
+      vec3(1, 0, 0), // 3 bottom right
+
+      vec3(0, 0, 1), // 4 bottom left
+      vec3(0, 1, 1), // 5 top left
+      vec3(1, 1, 1), // 6 top right
+      vec3(1, 0, 1), // 7 bottom right
+
+  };
 
   std::vector<vec2> texCoords = {
-      vec2(1.0f, 1.0f), // lower-left corner
-      vec2(1.0f, 0.0f), // lower-right corner
-      vec2(0.0f, 0.0f), // top-center corner
-      vec2(0.0f, 1.0f)};
+      vec2(0, 0),
+      vec2(0, 1),
+      vec2(1, 1),
+      vec2(1, 0),
+
+      vec2(0, 0),
+      vec2(0, 1),
+      vec2(1, 1),
+      vec2(1, 0),
+  };
 
   texture = new Texture("resources/container.jpg");
   texture->BindTexture();
@@ -65,13 +94,15 @@ void Renderer::Render()
   mainShader->Bind();
 
   glm::mat4 model = glm::mat4(1.0f);
-  model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+  model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 0.0f));
   ;
   glm::mat4 view = glm::mat4(1.0f);
   view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
   glm::mat4 projection;
-  projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+  // fov / aspectratio/ near clip / far clip TODO: add option for these things TODO: add aspect ration height/width
+  projection = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
 
   mainShader->setMat4f("model", model);
   mainShader->setMat4f("view", view);
@@ -83,7 +114,7 @@ void Renderer::Render()
   // mainShader->setVec4f("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
 
   // this draws using the incides, 6 is number of indices, gl unsined int is type
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 
   glfwSwapBuffers(window);
 }
