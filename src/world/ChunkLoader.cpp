acb8d677546962a27e85add8af69cf25829558e0
Shahan Neda <shahan.neda@gmail.com>
2020-09-03 17:19:32 -0700

    setup chunk loading storage map

--- src/world/ChunkLoader.cpp
+++ src/world/ChunkLoader.cpp
@@ -1,37 +1,43 @@
 
+#include <algorithm>
 #include "ChunkLoader.h"
 #include "Chunk.h"
-#include "glm/glm.hpp"
 #include "../graphics/WorldRenderer.h"
+#include <iterator>
+
 using glm::vec3;
+using std::pair;
 using std::vector;
 
 ChunkLoader::ChunkLoader(WorldRenderer *renderer) : loadedChunks()
 {
     this->renderer = renderer;
     Chunk *mainChunk = new Chunk(vec3(0, 0, 0));
-    loadedChunks.push_back(mainChunk);
+    loadedChunks.insert(std::pair<glm::vec3, Chunk *>(mainChunk->pos, mainChunk));
+    nonGeneratedChunks.push_back(mainChunk);
 }
 
 void ChunkLoader::NextChunkGenerationCycle()
 {
     std::cout << "generating chunks" << std::endl;
-    int lengthOfLoadedChunksAtStart = loadedChunks.size();
-    for (int i = 0; i < lengthOfLoadedChunksAtStart; i++)
+    vector<Chunk *> newNonGeneratedChunks; // store it here for temp, since we dont want the size of it actuallly changing
+    for (auto it = nonGeneratedChunks.begin(); it != nonGeneratedChunks.end();)
     {
-        Chunk *c = loadedChunks[i];
+        Chunk *c = *it;
         if (c->positiveXNeighber == nullptr)
         {
             Chunk *newChunk = new Chunk(vec3(c->pos.x + 1, c->pos.y, c->pos.z));
             c->positiveXNeighber = newChunk;
-            loadedChunks.push_back(newChunk);
+            loadedChunks.insert(pair<vec3, Chunk *>(newChunk->pos, newChunk));
+            newNonGeneratedChunks.push_back(newChunk);
             newChunk->negativeXNeighber = c;
         }
         if (c->negativeXNeighber == nullptr)
         {
             Chunk *newChunk = new Chunk(vec3(c->pos.x - 1, c->pos.y, c->pos.z));
             c->negativeXNeighber = newChunk;
-            loadedChunks.push_back(newChunk);
+            loadedChunks.insert(pair<vec3, Chunk *>(newChunk->pos, newChunk));
+            newNonGeneratedChunks.push_back(newChunk);
             newChunk->positiveXNeighber = c;
         }
 
@@ -39,14 +45,16 @@ void ChunkLoader::NextChunkGenerationCycle()
         {
             Chunk *newChunk = new Chunk(vec3(c->pos.x, c->pos.y + 1, c->pos.z));
             c->positiveYNeighber = newChunk;
-            loadedChunks.push_back(newChunk);
+            loadedChunks.insert(pair<vec3, Chunk *>(newChunk->pos, newChunk));
+            newNonGeneratedChunks.push_back(newChunk);
             newChunk->negativeYNeighber = c;
         }
         if (c->negativeYNeighber == nullptr)
         {
             Chunk *newChunk = new Chunk(vec3(c->pos.x, c->pos.y - 1, c->pos.z));
             c->negativeYNeighber = newChunk;
-            loadedChunks.push_back(newChunk);
+            loadedChunks.insert(pair<vec3, Chunk *>(newChunk->pos, newChunk));
+            newNonGeneratedChunks.push_back(newChunk);
             newChunk->positiveYNeighber = c;
         }
 
@@ -54,30 +62,38 @@ void ChunkLoader::NextChunkGenerationCycle()
         {
             Chunk *newChunk = new Chunk(vec3(c->pos.x, c->pos.y, c->pos.z + 1));
             c->positiveZNeighber = newChunk;
-            loadedChunks.push_back(newChunk);
+            loadedChunks.insert(pair<vec3, Chunk *>(newChunk->pos, newChunk));
+            newNonGeneratedChunks.push_back(newChunk);
             newChunk->negativeZNeighber = c;
         }
         if (c->negativeZNeighber == nullptr)
         {
             Chunk *newChunk = new Chunk(vec3(c->pos.x, c->pos.y, c->pos.z - 1));
             c->negativeZNeighber = newChunk;
-            loadedChunks.push_back(newChunk);
+            loadedChunks.insert(pair<vec3, Chunk *>(newChunk->pos, newChunk));
+            newNonGeneratedChunks.push_back(newChunk);
             newChunk->positiveZNeighber = c;
         }
 
         if (c->hasAllNeighbers() && !c->meshData.generated)
         {
+            it = nonGeneratedChunks.erase(it); // remove in a way that doesnt mess up the iterator
             c->meshData.GenerateData();
             renderer->renderedChunks.push_back(c);
         }
+        else
+        {
+            ++it;
+        }
     }
 
+    nonGeneratedChunks.insert(nonGeneratedChunks.end(), newNonGeneratedChunks.begin(), newNonGeneratedChunks.end()); // add the new chuncks to the  non generated chunks
     // mainChunk->meshData.GenerateData();
 }
 ChunkLoader::~ChunkLoader()
 {
-    for (Chunk *c : loadedChunks)
+    for (auto it = loadedChunks.begin(); it != loadedChunks.end(); ++it)
     {
-        delete c;
+        delete it->second;
     }
 }
--- src/world/ChunkLoader.h
+++ src/world/ChunkLoader.h
@@ -3,13 +3,17 @@
 // #include "../graphics/WorldRenderer.h"
 
 #include <vector>
+#include <unordered_map>
+#include <glm/gtx/hash.hpp>
+#include <glm/glm.hpp>
 class WorldRenderer; // forward dec
 class Chunk;         // forward dec
 class ChunkLoader
 {
 public:
     ChunkLoader(WorldRenderer *renderer);
-    std::vector<Chunk *> loadedChunks;
+    std::unordered_map<glm::vec3, Chunk *> loadedChunks;
+    std::vector<Chunk*> nonGeneratedChunks;
     void NextChunkGenerationCycle();
     ~ChunkLoader();
 
